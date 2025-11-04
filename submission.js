9d5beb79aeeb01d22af22b38a0a608f17cfb11ca
Eric Tam <eytam619@gmail.com>
2025-11-03 22:45:13 -0500

    Update submission.js

--- submission.js
+++ submission.js
@@ -1,17 +1,55 @@
 const findSum = function(array) {
-  // your code here - don't forget to return a number!
+  let result = array.reduce((a,b) => a + b, 0)
+  
+  return result
 };
 
 const findFrequency = function(array) {
-  // your code here - don't forget to return an object!
+  let tallyObj = {};
+  let result = {};
+  let most = 0;
+  let least = 0;
+  
+  for (let i = 0; i < array.length; i++) {
+    if (tallyObj[array[i]]) {
+      tallyObj[array[i]] += 1;
+    } else {
+      tallyObj[array[i]] = 1;
+    }
+  }
+  
+  for (let key in tallyObj) {
+    if (tallyObj[key] > most) {
+      most = tallyObj[key]
+      result.most = key;
+    } else {
+      least = tallyObj[key]
+      result.least = key;
+    }
+  }
+  
+  return result;
 };
 
 const isPalindrome = function(str) {
-  // your code here - don't forget to return a boolean!
+  let lowerCaseStr = str.toLowerCase();
+  let revStr = lowerCaseStr.split("").reverse().join("");
+  
+ if (lowerCaseStr !== revStr) {
+     return false;
+     };
+  return true
 };
 
 const largestPair = function(array) {
-  // your code here - don't forget to return a number!
+   let result = 0;
+  
+  for(let i = 0; i < array.length; i++) {
+    if (array[i] * array[i+1] > result) {
+      result = array[i] * array[i+1]
+    }
+  }
+  return result;
 };
 
 const removeParenth = function(str) {
